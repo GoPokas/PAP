e289ccc2e54c1ff485d2b91536eff38355bbba89
Rodrigo Alves <pocas.alves@gmail.com>
2022-01-20 16:59:09 +0000

    Small updates

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -16,7 +16,7 @@ $row = mysqli_fetch_assoc($result);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="./imgs/myicon.ico" type="image/x-icon">
+    <!-- <link rel="shortcut icon" href="../imgs/favicon.ico" type="image/x-icon"> -->
 </head>
 
 <body class="font-inter">
