8b83def4baf7af94262417c52108a1f8aa390187
Rodrigo Alves <pocas.alves@gmail.com>
2022-01-20 16:58:50 +0000

    Working FavIcon

--- components/footer.php
+++ components/footer.php
@@ -15,7 +15,6 @@ include('../components/sidebar.php');
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>LeaviFy</title>
-  <link rel="shortcut icon" href="./imgs/favicon.ico" type="image/x-icon">
 </head>
 
 <body>
