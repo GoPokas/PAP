6075649a1675655208fd960d937133c0d11a1ce9
Rodrigo Alves <pocas.alves@gmail.com>
2022-01-27 16:42:33 +0000

    Update

--- components/footer.php
+++ components/footer.php
@@ -4,26 +4,8 @@ require_once "../user/login_verify.php"; // Check if user is logged in
 include "../components/header.php";
 include "../components/sidebar.php";
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>LeaviFy</title>
-</head>
-
-<body>
-  <footer></footer>
-</body>
+<title>LeaviFy</title>
 <script src="../https://cdn.tailwindcss.com"></script>
 <script src="../js/jquery-3.6.0.min.js"></script>
 <script src="../js/loader.js"></script>
 <script src="https://unpkg.com/@themesberg/flowbite@1.2.0/dist/datepicker.bundle.js"></script>
-
-
-
-</html>
