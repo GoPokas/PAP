cc9413f5b8bc3ab156e26fd366cfeac9f12e8378
Rodrigo Alves <pocas.alves@gmail.com>
2022-02-04 09:26:01 +0000

    Button on hover color change

--- pages/requests.php
+++ pages/requests.php
@@ -1,14 +1,12 @@
 <?php
+include "../components/footer.php";
 
 $sql = "SELECT * FROM funcionario
-        INNER JOIN marcacao ON funcionario
         WHERE funcionario.id = '{$_SESSION["numFuncionario"]}'";
 
 $result = mysqli_query($conn, $sql);
 
 $row = mysqli_fetch_assoc($result);
-
-include "../components/footer.php";
 ?>
 
 <!DOCTYPE html>
@@ -45,7 +43,7 @@ include "../components/footer.php";
                                         <div class="bg-red-300 rounded-xl text-red-500 font-bold p-1">Recusado</div>
                                     </td>
                                     <td>
-                                        <button class="items-end opacity-70">
+                                        <button class="items-end opacity-70 hover:opacity-100">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                                 <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
                                                 <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
