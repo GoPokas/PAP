ceb008335e12fc598d8fd0eb62e31230d33fe753
Rodrigo Alves <pocas.alves@gmail.com>
2022-02-24 17:01:35 +0000

    Update

--- index.php
+++ index.php
@@ -51,5 +51,6 @@
         </div>
     </div>
 </body>
+<script src="https://cdn.tailwindcss.com"></script>
 
 </html>
--- js/loader.js
+++ js/loader.js
@@ -2,6 +2,5 @@ let preloader = $("#preloader");
 if (preloader) {
   window.addEventListener("load", () => {
     preloader.remove();
-    console.log("preloader removed");
   });
 }
