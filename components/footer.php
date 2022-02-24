4efd93fce9b5b03b4c8cb81e06261924e3a02182
Rodrigo Alves <pocas.alves@gmail.com>
2022-02-24 10:27:03 +0000

    Updates

--- components/footer.php
+++ components/footer.php
@@ -5,7 +5,8 @@ include "../components/header.php";
 include "../components/sidebar.php";
 ?>
 <title>LeaviFy</title>
-<link rel="stylesheet" href="../css/tailwind.css">
+<link rel="stylesheet" href="../css/style.css">
 <script src="../js/jquery-3.6.0.min.js"></script>
 <script src="../js/loader.js"></script>
 <script src="https://unpkg.com/@themesberg/flowbite@1.2.0/dist/datepicker.bundle.js"></script>
+<script src="https://cdn.tailwindcss.com"></script>
