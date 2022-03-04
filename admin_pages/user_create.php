cad33f2664a86c5ed104edaa9b5a9c56ea824074
Rodrigo Alves <pocas.alves@gmail.com>
2022-03-04 17:00:49 +0000

    Updates

--- admin_pages/user_create.php
+++ admin_pages/user_create.php
@@ -8,10 +8,21 @@ include "../components/footer.php"; ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="../imgs/myIcon.ico" type="image/x-icon">
 </head>
 
 <body>
+    <div class="h-full w-[80%] relative overflow-hidden lg:ml-60 top-14">
+        <div class="w-[95%] grid grid-cols-1 gap-4">
+            <div class="p-8">
+                <span class="text-2xl sm:text-4xl leading-none font-bold text-gray-900 pb-1 pr-3">Criação de funcionário</span>
+            </div>
+            <div class="bg-white rounded-lg shadow-lg">
+                    <div class="flex items-center justify-between mb-4 top-2">
+                    </div>
+                    <form action=""></form>
+            </div>
+        </div>
+    </div>
 
 </body>
 
