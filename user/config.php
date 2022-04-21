44ccd21eddc1fa06d8fdeebd87235ed8d9bf37c2
Rodrigo Alves <59261343+GoPokas@users.noreply.github.com>
2022-04-21 08:56:10 +0100

    Updates

--- user/config.php
+++ user/config.php
@@ -6,7 +6,7 @@
 // define('DB_PASSWORD', );
 // define('DB_NAME', );
 
-($conn = mysqli_connect("127.0.0.1", "root", "", "demo")) or
+($conn = mysqli_connect("127.0.0.1", "root", "pokinhas8", "demo")) or
   die("Connection failed: " . mysqli_connect_error());
 
 $conn->query("SET NAMES utf8");
